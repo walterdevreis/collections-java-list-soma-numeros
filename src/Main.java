0a5559bef0138b8b69b88b9b9025f0f5cb85974c
walter dev reis <walter-reis@outlook.com>
2025-09-17 15:23:36 -0300

    Projeto testado e entrege

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
+import entity.SomaNumeros;
+
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        SomaNumeros somaNumeros = new SomaNumeros();
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(10);
+        somaNumeros.adicionarNumero(15);
+        somaNumeros.adicionarNumero(20);
+        somaNumeros.adicionarNumero(25);
+        somaNumeros.adicionarNumero(30);
+        somaNumeros.adicionarNumero(35);
+        somaNumeros.adicionarNumero(40);
+        somaNumeros.adicionarNumero(45);
+        somaNumeros.adicionarNumero(50);
+
+        System.out.println("-------------------------------------------");
+
+        int soma = somaNumeros.calcularSoma();
+        System.out.println("A soma dos números da lista: " + soma);
+
+        System.out.println("-------------------------------------------");
+
+        int maiorNumero = somaNumeros.encontrarMaiorNumero();
+        System.out.println("O maior número da lista: " + maiorNumero);
+
+        System.out.println("-------------------------------------------");
+
+        int menorNumero = somaNumeros.encontrarMenorNumero();
+        System.out.println("O menor número da lista: " + menorNumero);
+
+        System.out.println("-------------------------------------------");
+
+        List<Integer> exibirNumeros = somaNumeros.exibirNumeros();
+        for (Integer number : exibirNumeros){
+            System.out.println(number);
         }
+
     }
 }
